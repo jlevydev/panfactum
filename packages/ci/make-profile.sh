f46b89ed3f390aa94107a52e3fdd518348a6584c
jclangst <jack@fullstackjack.io>
2023-08-29 19:22:43 -0400

    fix: aws profile sed

--- packages/ci/make-profile.sh
+++ packages/ci/make-profile.sh
@@ -13,5 +13,5 @@ PROFILE="$(readlink -f ~/.devenv/profile)"
 echo "PATH=$PROFILE/bin:$PROFILE/sbin:\$PATH" >> ~/.profile
 
 # Fix-up AWS profiles
-echo "sed -i \"s/@role_arn@/\${AWS_ROLE_ARN//\//\\/}/g\" /home/runner/.aws/config" >> ~/.profile
-echo "sed -i \"s/@role_session_name@/\${RUNNER_NAME//\//\\/}/g\" /home/runner/.aws/config" >> ~/.profile
+echo "sed -i \"s/@role_arn@/\${AWS_ROLE_ARN//\//\\\\/}/g\" /home/runner/.aws/config" >> ~/.profile
+echo "sed -i \"s/@role_session_name@/\${RUNNER_NAME//\//\\\\/}/g\" /home/runner/.aws/config" >> ~/.profile
